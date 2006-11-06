4337c290ca66bc3202273a49b10f73e420baeb43
daedalus <daedalus@dd8176a4-830e-0410-a43c-b1af7eaea744>
2006-11-06 22:39:17 +0000

    Changed license to MIT license. Updated copyright to correctly reflect ownership by Justin Warren rather than Unicity.

--- setup.py
+++ setup.py
@@ -3,29 +3,15 @@
 # $Revision$
 #
 #    libsnmp - a Python SNMP library
-#    Copyright (C) 2003 Unicity Pty Ltd <libsnmp@unicity.com.au>
-#
-#    This library is free software; you can redistribute it and/or
-#    modify it under the terms of the GNU Lesser General Public
-#    License as published by the Free Software Foundation; either
-#    version 2.1 of the License, or (at your option) any later version.
-#
-#    This library is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#    Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public
-#    License along with this library; if not, write to the Free Software
-#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#    Copyright (c) 2003 Justin Warren <daedalus@eigenmagic.com>
 
 from distutils.core import setup
 
 import time
 
-version_major = 1
+version_major = 2
 version_minor = 0
-version_build = 3
+version_build = 0
 version_devel=''
 #version_devel='-dev-' + time.strftime('%Y-%m-%d-%H%M')
 
@@ -34,11 +20,11 @@ version='%d.%d.%d%s' % (version_major, version_minor, version_build, version_dev
 setup(
     name='libsnmp',
     version=version,
-    description='An SNMP library',
+    description='A Python SNMP library',
     author='Justin Warren',
-    author_email='justin.warren@unicity.com.au',
-    license='Proprietary',
-    url='http://www.unicity.com.au/',
+    author_email='daedalus@eigenmagic.com',
+    license='MIT',
+    url='http://www.eigenmagic.com',
     packages=['libsnmp'],
     package_dir = { '':'lib'},
     
